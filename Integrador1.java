c353ac3fb5a90458bcd5319f394ec67e3be41b57
Braynin <brayhannb@gmail.com>
2024-06-28 17:58:44 -0500

    colocando mensaje de error al mandar letras

--- Integrador1.java
+++ Integrador1.java
@@ -11,26 +11,32 @@ public class Integrador1 {
                                                 : "chamare, no era par y si querias que fuera impar ps bien por ti oniichan UwU!!"));
 
                 System.out.print("Holiwis brou, dame tu primer numerito porfis --> ");
-                int num1 = sc.nextInt();
-                System.out.print("Arigatou oniichan, ahora no pares y dame el segundo oniichan UwU --> ");
-                int num2 = sc.nextInt();
-                System.out.println(
-                                "Weno, ahora que? :v , dime que hacer oniichan!!! UnU -->\n1 -- Sumar\n2 -- Restar\n3 -- Multiplicar\n4 -- Dividir");
-                int operacion = sc.nextInt();
-                if (operacion == 4)
-                        if (num2 == 0) {
-                                System.out.println("chamare, no se puede dividir por 0 oniichan!!");
-                                System.exit(0);
-                        } else
-                                System.out.print("Aversh!! segun lo que me pediste, el resultado de la division");
-                else
-                        System.out.print("Aversh!! segun lo que me pediste, el resultado de la "
-                                        + (operacion == 1 ? "suma"
-                                                        : (operacion == 2 ? "resta"
-                                                                        : "multiplicacion")));
-                System.out.println(" es " + (operacion == 1 ? num1 + num2
-                                : (operacion == 2 ? num1 - num2
-                                                : (operacion == 3 ? num1 * num2 : num1 / num2))));
+                Integer num1 = sc.nextInt();
+                if (num1 instanceof Integer) {
+                        System.out.print("Arigatou oniichan, ahora no pares y dame el segundo oniichan UwU --> ");
+                        int num2 = sc.nextInt();
+                        System.out.println(
+                                        "Weno, ahora que? :v , dime que hacer oniichan!!! UnU -->\n1 -- Sumar\n2 -- Restar\n3 -- Multiplicar\n4 -- Dividir");
+                        int operacion = sc.nextInt();
+                        if (operacion == 4)
+                                if (num2 == 0) {
+                                        System.out.println("chamare, no se puede dividir por 0 oniichan!!");
+                                        System.exit(0);
+                                } else
+                                        System.out.print(
+                                                        "Aversh!! segun lo que me pediste, el resultado de la division");
+                        else
+                                System.out.print("Aversh!! segun lo que me pediste, el resultado de la "
+                                                + (operacion == 1 ? "suma"
+                                                                : (operacion == 2 ? "resta"
+                                                                                : "multiplicacion")));
+                        System.out.println(" es " + (operacion == 1 ? num1 + num2
+                                        : (operacion == 2 ? num1 - num2
+                                                        : (operacion == 3 ? num1 * num2 : num1 / num2))));
+                } else {
+                        System.out.println(
+                                        "Escribe un numero porfis, no se puede realizar ninguna operacion sin numeros UnU");
+                }
                 sc.close();
         }
 }
