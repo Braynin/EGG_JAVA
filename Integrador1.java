b9e142314a18d635d94742907c638d7e2ff084b2
Braynin <brayhannb@gmail.com>
2024-07-01 19:45:24 -0500

    actualizando local

--- Integrador1.java
+++ Integrador1.java
@@ -3,16 +3,13 @@ import java.util.Scanner;
 public class Integrador1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        /*
-         * System.out.print("Holi bro, dame un numerito pa analizarlo UwU --> ");
-         * int numero = sc.nextInt();
-         * System.out.println(
-         * "Vamo a versh si tu numerito es par papu OwO --> " + (numero % 2 == 0 ?
-         * "a wevo , si es par oniichan!!"
-         * :
-         * "chamare, no era par y si querias que fuera impar ps bien por ti oniichan UwU!!"
-         * ));
-         */
+
+        System.out.print("Holi bro, dame un numerito pa analizarlo UwU --> ");
+        int numero = sc.nextInt();
+        System.out.println(
+                "Vamo a versh si tu numerito es par papu OwO --> " + (numero % 2 == 0 ? "a wevo , si es par oniichan!!"
+                        : "chamare, no era par y si querias que fuera impar ps bien por ti oniichan UwU!!"));
+
         System.out.print("Holiwis brou, dame tu primer numerito porfis --> ");
         int num1 = sc.nextInt();
         System.out.print("Arigatou oniichan, ahora no pares y dame el segundo oniichan UwU --> ");
@@ -26,6 +23,5 @@ public class Integrador1 {
                 + " es " + (operacion == 1 ? num1 + num2
                         : (operacion == 2 ? num1 - num2 : (operacion == 3 ? num1 * num2 : num1 / num2))));
         sc.close();
-        System.out.println(num1);
     }
 }
